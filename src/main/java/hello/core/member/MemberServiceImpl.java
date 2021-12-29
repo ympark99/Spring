51a1d6360788818ffd6e3da7d54d10ce7e8f769b
ympark99 <ympark1230@naver.com>
2021-12-29 15:28:41 +0900

    메인 함수 개발, 테스트 코드 제작

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); // dip 위반
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,10 +1,11 @@
 package hello.core.member;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository{
 
-    private static Map<Long, Member> store = new HashMap<Long, Member>();
+    private static Map<Long, Member> store = new HashMap<>();
 
     @Override
     public void save(Member member) {
