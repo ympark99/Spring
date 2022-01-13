decb991f65664ee3f7b5b5ba89d9584f7608aa34
ympark99 <ympark1230@naver.com>
2022-01-13 12:56:06 +0900

    탐색 위치, 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.FilterType;
 // @Component 찾아서 자동 등록
 @ComponentScan(
         // AppConfig의 Configuration 제외하기 위해 등록
+        // basePackages = "hello.core.member", // 탐색할 패키지 시작 위치 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 
