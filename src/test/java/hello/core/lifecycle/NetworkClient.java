86372888f31f51b61715f6e81bf62f6399c39f12
ympark99 <ympark1230@naver.com>
2022-01-31 11:38:06 +0900

    초기화, 소멸 메소드

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자 호출, url =" + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +27,20 @@ public class NetworkClient {
     // 서비스 종료 시 호출
     public void disconnect(){
         System.out.println("close " + url);
+    }
+
+    // 의존 관계 주입 종료 후 실행
+    @Override
+    public void afterPropertiesSet() throws Exception{
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 소멸 메소드
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
